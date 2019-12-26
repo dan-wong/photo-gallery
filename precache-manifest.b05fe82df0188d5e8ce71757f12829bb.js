4131e2d1f3016583a8f6c7359514315e64ec445a
Daniel <dan_wong@outlook.com>
2019-12-26 14:06:16 +1300

    Updates

---
+++ precache-manifest.b05fe82df0188d5e8ce71757f12829bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a666b116f6636f793bb9435094d82d",
+    "url": "/dan-wong/photo-gallery/index.html"
+  },
+  {
+    "revision": "450a2ef00f46662d080f",
+    "url": "/dan-wong/photo-gallery/static/css/main.b2c7c061.chunk.css"
+  },
+  {
+    "revision": "86333e5f74feab68454d",
+    "url": "/dan-wong/photo-gallery/static/js/2.dfb20aee.chunk.js"
+  },
+  {
+    "revision": "fc8bc21d8392cbf38ae828c75bee2e0a",
+    "url": "/dan-wong/photo-gallery/static/js/2.dfb20aee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "450a2ef00f46662d080f",
+    "url": "/dan-wong/photo-gallery/static/js/main.59c6f825.chunk.js"
+  },
+  {
+    "revision": "de14de86a230fc4c0d2f",
+    "url": "/dan-wong/photo-gallery/static/js/runtime-main.4673ce85.js"
+  }
+]);
